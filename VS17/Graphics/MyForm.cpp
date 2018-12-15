01909fa4a718972312f4c8857d6a5f58956b83a9
Vitaly Dobrohotov <19dobrohotov99@mail.ru>
2018-12-15 20:30:05 +0300

    files for graphics

---
+++ VS17/Graphics/MyForm.cpp
@@ -0,0 +1,14 @@
+#include "MyForm.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+
+int main(array <String^ > ^ args) {
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	Graphics::MyForm form;
+	Application::Run(%form);
+	return 0;
+}
+
